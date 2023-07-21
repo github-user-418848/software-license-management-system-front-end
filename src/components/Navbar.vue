9c77158b32923e2cb9ba959fb351b3cc7768c742
github-user-418848 <x1cjlyjr@duck.com>
2023-07-21 18:08:42 +0800

    added toggle dark theme functionality

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,23 +2,67 @@
     <nav class="navbar bg-primary text-secondary">
         <div class="container-fluid d-flex justify-content-space-between">
             <img class="img-fluid navbar-brand" :src="imgSrc" alt="MSSC" width="175">
-            <div class="btn btn-primary d-lg-block d-none" id="theme-toggle">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
-                    class="bi bi-brightness-high-fill" viewBox="0 0 16 16">
-                    <path
-                        d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
-                </svg>
-            </div>
+            <button class="btn btn-primary d-lg-block d-none" @click="toggleTheme" id="theme-toggle">
+                <template v-if="toggle">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                        class="bi bi-moon-fill" viewBox="0 0 16 16">
+                        <path
+                            d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z" />
+                    </svg>
+                </template>
+                <template v-else>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                        class="bi bi-brightness-high-fill" viewBox="0 0 16 16">
+                        <path
+                            d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
+                    </svg>
+                </template>
+            </button>
         </div>
     </nav>
 </template>
 <script>
 export default {
     name: 'Navbar',
+    data() {
+        return {
+            toggle: false,
+        }
+    },
+    mounted() {
+        this.initializeTheme();
+    },
     computed: {
         imgSrc() {
             return `${import.meta.env.VITE_BACKEND_BASE_PATH_STATIC}/img/mssc.png`;
         },
+        token() {
+            return this.$store.state.token;
+        },
+        loggedIn() {
+            return this.$store.state.loggedIn;
+        },
+    },
+    methods: {
+        initializeTheme() {
+            if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                this.toggle = true;
+                this.setTheme();
+            }
+        },
+        toggleTheme() {
+            this.toggle = !this.toggle;
+            this.setTheme();
+        },
+        setTheme() {
+            if (this.toggle) {
+                document.documentElement.setAttribute('data-bs-theme', 'dark');
+                document.querySelector('.g-recaptcha').setAttribute('data-theme', 'dark');
+            } else {
+                document.documentElement.setAttribute('data-bs-theme', 'light');
+                // document.querySelector('.g-recaptcha').setAttribute('data-theme', 'light');
+            }
+        }
     }
 }
 </script>
