27b1f5b399674d4fe9e314d7a0426dad45293447
github-user-418848 <x1cjlyjr@duck.com>
2023-07-21 12:09:06 +0800

    refactored code

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
-import './main.css'
+import './assets/css/main.css'
 import App from './App.vue'
 import router from './router'
-import store from './store';
+import store from './store'
 
 
 const app = createApp(App);
